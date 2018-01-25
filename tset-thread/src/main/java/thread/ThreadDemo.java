8bc700322582e26fc75d24b5c17f5417114014f1
chenxuewen <852327204@qq.com>
2018-01-25 23:56:52 +0800

    add thread2

--- tset-thread/src/main/java/thread/ThreadDemo.java
+++ tset-thread/src/main/java/thread/ThreadDemo.java
@@ -23,9 +23,32 @@ class NewThread implements Runnable {
     }
 }
 
+class NewThread2 extends Thread {
+    NewThread2() {
+        // 创建第二个新线程
+        super("Demo Thread");
+        System.out.println("Child thread: " + this);
+        start(); // 开始线程
+    }
+
+    // 第二个线程入口
+    public void run() {
+        try {
+            for(int i = 5; i > 0; i--) {
+                System.out.println("Child Thread: " + i);
+                // 让线程休眠一会
+                Thread.sleep(50);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Child interrupted.");
+        }
+        System.out.println("Exiting child thread.");
+    }
+}
+
 public class ThreadDemo {
     public  static void main(String []args) {
-        new NewThread(); // 创建一个新线程
+        new NewThread2(); // 创建一个新线程
         try {
             for(int i = 5; i > 0; i--) {
                 System.out.println("Main Thread: " + i);
